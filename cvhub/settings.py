5dd140a25a8be7081083db1602a1bd5efd00a841
Lane Miles <lmiles1234@gmail.com>
2015-10-05 07:29:16 +0000

    more agin

--- cvhub/settings.py
+++ cvhub/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cvhub_app',
 )
 
 MIDDLEWARE_CLASSES = (
--- cvhub/urls.py
+++ cvhub/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^cvhub_app/', include('cvhub_app.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
