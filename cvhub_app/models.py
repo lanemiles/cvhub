f49b379f7ab07f1c7813b3855728fc286b70e46c
Lane Miles <lmiles1234@gmail.com>
2015-10-31 14:28:50 -0700

    starting model

--- cvhub_app/models.py
+++ cvhub_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class CommentableResumeItem(models.Model):
+    """
+    Highest level abstract base class for commentable items
+    """
+    order = models.IntegerField()
+    enabled = models.BooleanField()
+
+    class Meta:
+        abstract = True
+
+
+class BulletPoint(CommentableResumeItem):
+    """
+    Abstract base class for different types of resume items
+    """
+
+    text = models.CharField(max_length=1024)
+
+
