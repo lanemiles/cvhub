8f027977b7f80e0493d41fa42563651b1018e6ec
Lane Miles <lmiles1234@gmail.com>
2015-11-01 23:45:08 +0000

    useless commit to get other files

--- cvhub_app/views.py
+++ cvhub_app/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from cvhub_app.forms import *
 
 
 def index(request):
     return render(request, 'current_time.html', {'question': 'his'})
+
+
+def create_user(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = UserInfoForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = UserInfoForm()
+
+    return render(request, 'create_user.html', {'form': form})
+
+
+def thanks(request):
+    return render(request, 'thanks.html', {})
