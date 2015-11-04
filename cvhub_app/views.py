efd05468afcd2bd7a1a45ccd68267a25b6a81ef7
Lane Miles <lmiles1234@gmail.com>
2015-11-04 06:53:32 +0000

    preparing for new file upload

--- cvhub_app/views.py
+++ cvhub_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from cvhub_app.forms import *
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -16,9 +17,21 @@ def create_user(request):
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
-            # ...
+            
+            # make the User object
+            user = User.objects.create_user(form.cleaned_data.get('email'), form.cleaned_data.get('email'), form.cleaned_data.get('password'))
+            user.first_name = form.cleaned_data.get('first_name') 
+            user.last_name = form.cleaned_data.get('last_name')
+            user.save()
+
+            # make the UserInfo object
+            user_wrapper = UserInfo()
+            user_wrapper.dob = form.cleaned_data.get('dob')
+            user_wrapper.user = user
+            user_wrapper.save()
+
             # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+            return render(request, 'thanks.html', {'user_wrapper': user_wrapper})
 
     # if a GET (or any other method) we'll create a blank form
     else:
@@ -29,3 +42,7 @@ def create_user(request):
 
 def thanks(request):
     return render(request, 'thanks.html', {})
+
+@login_required
+def user_profile(request):
+    return render(request, 'profile.html', {'user': request.user })
